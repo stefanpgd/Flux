0daae8fbf27caf4a96aa1d44f8b1e6e5abc64e29
Stefan Pijnacker <stefanpijnacker@gmail.com>
2024-04-17 13:02:23 +0200

    🗄️ Dependencies like ImGui & D3DCompiler setup

---
+++ Source/Flux.cpp
@@ -0,0 +1,1 @@
+#include "Flux.h"
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+// TODO:
+// Extract some of the Nova files, find out what I need to get a window running
+
 int main()
 {
 	printf("Hello World\n");
