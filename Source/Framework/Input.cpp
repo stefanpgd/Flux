c4a20d64c6de282a71acd4d794be6b77ae33ddcf
Stefan Pijnacker <stefanpijnacker@gmail.com>
2024-04-22 19:20:05 +0200

    ⌨️ Added an Input system using ImGui as backend

---
+++ Source/Framework/Input.cpp
@@ -0,0 +1,31 @@
+#include "Framework/Input.h"
+
+bool Input::GetKey(KeyCode key)
+{
+	return ImGui::IsKeyDown(ImGuiKey(key));
+}
+
+bool Input::GetMouseButton(MouseCode button)
+{
+	return ImGui::GetIO().MouseDown[unsigned int(button)];
+}
+
+int Input::GetMouseX()
+{
+	return ImGui::GetIO().MousePos.x;
+}
+
+int Input::GetMouseY()
+{
+	return ImGui::GetIO().MousePos.y;
+}
+
+int Input::GetMouseVelocityX()
+{
+	return ImGui::GetIO().MouseDelta.x;
+}
+
+int Input::GetMouseVelocityY()
+{
+	return ImGui::GetIO().MouseDelta.y;
+}
